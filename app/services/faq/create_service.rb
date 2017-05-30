1b492835804ff5a4bfc039f91048c96017c80c38
Sudok <glauber.gw@gmail.com>
2017-05-30 18:53:30 -0300

    adjusting symbols for test services

--- app/services/faq/create_service.rb
+++ app/services/faq/create_service.rb
@@ -3,20 +3,24 @@ module FaqModule
     def initialize(params)
       # TODO: identify origin and set company
       @company = Company.last
-      @question = params["question-original"]
-      @answer = params["answer-original"]
-      @hashtags = params["hashtags-original"]
+      @question = params['question-original']
+      @answer = params['answer-original']
+      @hashtags = params['hashtags-original']
     end
 
     def call
-      Faq.transaction do
-        faq = Faq.create(question: @question, answer: @answer, company: @company)
-        return "Hashtag Obrigatória" if @hashtags == nil
-        @hashtags.split(/[\s,]+/).each do |hashtag|
-          faq.hashtags << Hashtag.create(name: hashtag)
+      return 'Hashtag Obrigatória' if @hashtags == nil || @hashtags == ''
+      begin
+        Faq.transaction do
+          faq = Faq.create(question: @question, answer: @answer, company: @company)
+          @hashtags.split(/[\s,]+/).each do |hashtag|
+            faq.hashtags << Hashtag.create(name: hashtag)
+          end
         end
+        'Criado com sucesso'
+      rescue
+        'Problemas na criação'
       end
-      "Criado com sucesso"
     end
   end
 end
